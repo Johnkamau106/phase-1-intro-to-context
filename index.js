6c78a8b5492d8caffaa4af423acbfc7cda10f36b
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-03-24 01:33:46 +0300

    function create employment names hours in recordswages

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(employeeRecord, dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    employeeRecord.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date: date
+    });
+    return employeeRecord;
+  }
+  
+  function createTimeOutEvent(employeeRecord, dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    employeeRecord.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date: date
+    });
+    return employeeRecord;
+  }
+  
+  function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+    
+    if (timeInEvent && timeOutEvent) {
+      return (timeOutEvent.hour - timeInEvent.hour) / 100;  
+    } else {
+      return 0;  
+    }
+  }
+  
+  function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    return hoursWorked * employeeRecord.payPerHour;
+  }
+  
+  function allWagesFor(employeeRecord) {
+    const datesWorked = employeeRecord.timeInEvents.map(event => event.date);
+    return datesWorked.reduce((totalWages, date) => {
+      return totalWages + wagesEarnedOnDate(employeeRecord, date);
+    }, 0);
+  }
+  
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employeeRecord) => {
+      return totalPayroll + allWagesFor(employeeRecord);
+    }, 0);
+  }
+  
